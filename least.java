b573a9cd3098c030b98e9a5ce71208a34afe8857
divya-kandhasamy <divya1314123@gmail.com>
2016-09-02 12:36:57 +0530

    Create least.java

---
+++ least.java
@@ -0,0 +1,30 @@
+import java.io.*;
+class least
+{
+public static void main(String args[])
+{
+int i=0;
+System.out.println("Enter the number");
+Scanner s=new Scanner(System.in);
+System.out.println("Enter the delete count");
+int n=s.next();
+int d=s.nextInt()
+int l=n.length();
+int a=Integer.parseInt(n);
+int array[]=new int array[l];
+while(n!=0)
+{
+int a[i++]=n%10;
+n=n/10;
+}
+Arrays.sort(array);
+for(i=0;i<l-d-1;i++)
+{
+Sys.out.println(a[i]);
+}
+}
+}
+
+
+
+
